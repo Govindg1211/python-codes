52239e8eac1d46ec7796da69b7bec09d38c428b5
Govind Gupta <govind.15986@stu.upes.ac.in>
2024-09-01 22:24:37 +0530

    final save of assignment

---
+++ code1.py
@@ -0,0 +1,6 @@
+import sys
+Name = input("Enter your name:")
+Course = input("Enter your course:")
+print("your name is:", Name)
+print("you are a student of:", Course)
+print("version", sys.version)
